90050a16d9c75b4a55dc4c1df95212743d19a37d
soaresgus <gustavosoaresbt@hotmail.com>
2022-07-15 17:27:01 -0300

    Added eslint + prettier;

--- web/src/components/CloseButton.tsx
+++ web/src/components/CloseButton.tsx
@@ -1,13 +1,13 @@
-import { Popover } from "@headlessui/react";
-import { X } from "phosphor-react";
+import { Popover } from '@headlessui/react';
+import { X } from 'phosphor-react';
 
 export function CloseButton() {
-    return (
-        <Popover.Button 
-        className="top-5 right-5 absolute button" 
-        title="Fechar formulário de feedback"
-        >
-            <X weight="bold" className="w-4 h-4" />
-        </Popover.Button>
-    );
+  return (
+    <Popover.Button
+      className="top-5 right-5 absolute button"
+      title="Fechar formulário de feedback"
+    >
+      <X weight="bold" className="w-4 h-4" />
+    </Popover.Button>
+  );
 }
--- web/src/components/ThemeButton.tsx
+++ web/src/components/ThemeButton.tsx
@@ -1,29 +1,21 @@
-import { Moon, Sun } from "phosphor-react";
-import { useState } from "react"
+import { Moon, Sun } from 'phosphor-react';
+import { useState } from 'react';
 
 export function ThemeButton() {
-    const [darkTheme, setDarkTheme] = useState(false);
+  const [darkTheme, setDarkTheme] = useState(false);
 
-    function handleSetTheme(theme: boolean) {
-        setDarkTheme(theme)
-        document.documentElement.classList.toggle('dark');
-    }
+  function handleSetTheme(theme: boolean) {
+    setDarkTheme(theme);
+    document.documentElement.classList.toggle('dark');
+  }
 
-    return (
-        <button onClick={() => handleSetTheme(!darkTheme)}>
-            {
-                darkTheme
-                ?
-                <Sun
-                    size={20}
-                    className='text-amber-400'
-                />
-                :
-                <Moon
-                    size={20}
-                    className='text-amber-400'
-                />
-            }
-        </button>
-    )
+  return (
+    <button onClick={() => handleSetTheme(!darkTheme)}>
+      {darkTheme ? (
+        <Sun size={20} className="text-amber-400" />
+      ) : (
+        <Moon size={20} className="text-amber-400" />
+      )}
+    </button>
+  );
 }
